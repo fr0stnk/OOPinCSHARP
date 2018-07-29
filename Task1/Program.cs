3f713a95181c87cf4ddd9287a984205b7d95aee7
fr0stnk <nik7966@icloud.com>
2018-07-29 12:40:25 +0300

    First Task Added

---
+++ Task1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace OOPinCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dialog> dialogs = new List<Dialog>();
+
+            dialogs.Add(new Dialog("Кто вы?", new[]
+                { "Человек", "Брандлмуха", "Кхаджит" }));
+            dialogs.Add(new Dialog("Что вы хотите?", new string[]
+                {"Победить Аразота", "Стать богатым", "Найти боевых товарищей"}));
+            dialogs.Add(new Dialog("Чем вы можете помочь ордену?", new string[]
+            { "Я отлчиный воин", "Я добротный маг", "Я могу работать в кузнице"}));
+
+            Console.WriteLine("Совершенно очевидно, что мы не берём в наш орден \nкого попало. По этому заполни вот эту анкету, " +
+                              "и мы \nпримем решение, брать тебя или нет...");
+            Console.WriteLine();
+
+            foreach (var item in dialogs )
+            {
+                item.DialogStart();
+            }
+
+            Console.ReadLine();
+        }
+
+       class Dialog
+        {
+            #region Fields
+            private string Question { get; }
+            private string[] Answers { get; }
+            public bool IsDoorOpen { get; set; }
+            
+            #endregion
+            
+            #region Constructor
+            public Dialog(string question, string[] answers)
+            {
+                Question = question;
+                Answers = answers;
+            }
+            #endregion
+
+            #region Methods
+            public void DialogStart()
+            {
+                Console.WriteLine(Question);
+                WriteAnswers();
+                Console.ReadLine();
+                Console.WriteLine();
+                IsDoorOpen = true;
+            }
+
+            private void WriteAnswers()
+            {
+                foreach (var item in Answers)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+            #endregion
+
+
+        }
+    }
+}
--- Task1/Properties/AssemblyInfo.cs
+++ Task1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OOPinCSHARP")]
+[assembly: AssemblyTitle("Task1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OOPinCSHARP")]
+[assembly: AssemblyProduct("Task1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d359c676-fff1-4d34-8a9d-25ec07d2e0bd")]
+[assembly: Guid("0a6fe736-aeaf-436f-89d7-7297c436e081")]
 
 // Version information for an assembly consists of the following four values:
 //
