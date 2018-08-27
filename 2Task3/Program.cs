f2890348f089cd326077a03a5b6b3b10a95a4276
fr0stnk <nik7966@icloud.com>
2018-08-27 20:34:23 +0300

    Initial Commit

---
+++ 2Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WebParser parser = new WebParser("google.com");
+            Console.WriteLine(parser.FindContentInTag("h1"));
+        }
+    }
+
+    class WebParser : WebClient
+    {
+        private string _url;
+
+        public WebParser(string url)
+        {
+            _url = url;
+        }
+
+        public string FindContentInTag(string tag)
+        {
+            string page = DownloadString(_url);
+            Regex exp = new Regex($"/<{tag}[^>] *>[^<] *<\/{tag}>/");
+
+            return exp.Match(page).Value;
+        }
+    }
+}
